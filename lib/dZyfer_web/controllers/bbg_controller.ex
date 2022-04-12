bb15ce60a18c71ffeb0f2fc97405cd0e52b2a2a0
Thomas Browne <tbrowne@suprabonds.com>
2022-04-12 10:26:28 +0200

    no more double send

--- lib/dZyfer_web/controllers/bbg_controller.ex
+++ lib/dZyfer_web/controllers/bbg_controller.ex
@@ -5,17 +5,11 @@ defmodule DZyferWeb.BbgController do
     IO.puts("<------data------->")
     IO.inspect params
     json(conn, %{data: params}) 
-    conn
-    |> put_status(:ok)
-    |> send_resp(200, "ok")
   end
 
   def ping(conn, _params) do
     IO.puts("<------ping------->")
     json(conn, %{time: :os.system_time(:milli_seconds)}) 
-    conn
-    |> put_status(:ok)
-    |> send_resp(200, "ok")
   end
 
 end
